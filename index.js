66aca2ebbd6e901882f6ec7ceeb908aed3ee0a10
fynnchen <ethan.cfchen@gmail.com>
2019-11-15 15:29:35 +0800

    Cleanup

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ import * as string from './src/stringProcessing';
 import Researcher from './src/researcher';
 import Paper from './src/values/Paper';
 
-/*
- * Everything exported here is put on the `yoast.analysis` global in the plugin.
- */
 export { Researcher, Paper, string };
