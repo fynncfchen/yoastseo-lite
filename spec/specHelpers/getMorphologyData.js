ba8956d6aca0c42ef4a178a6bf9c0f078143fc1f
fynnchen <ethan.cfchen@gmail.com>
2019-11-15 15:06:03 +0800

    Fork from yoastseo

---
+++ spec/specHelpers/getMorphologyData.js
@@ -0,0 +1,23 @@
+import en from "../../premium-configuration/data/morphologyData-v2.json";
+import de from "../../premium-configuration/data/morphologyData-de-v3.json";
+
+
+const morphologyData = {
+	en,
+	de,
+};
+
+/**
+ * Requires morphology data. To be used in the analysis to recognize different word forms.
+ *
+ * @param {string} language The language for which to load the morphology data.
+ *
+ * @returns {Object} The morphology data.
+ */
+export default function getMorphologyData( language ) {
+	if ( morphologyData[ language ] ) {
+		return morphologyData[ language ];
+	}
+
+	return {};
+}
